9b8def3657d6751e653bae5bc0892bf76c981abb
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-05-31 22:18:24 +0200

    fix(service) changing url in mail content

--- src/SharedKernel/Service/RecoveryPasswordMailer.php
+++ src/SharedKernel/Service/RecoveryPasswordMailer.php
@@ -2,7 +2,6 @@
 
 namespace App\SharedKernel\Service;
 
-use App\Domain\AppUser\Entity\AppUser;
 use App\Domain\AppUser\Entity\RecoveryPassword;
 use Symfony\Component\Mailer\MailerInterface;
 
@@ -11,17 +10,18 @@ class RecoveryPasswordMailer extends CoreMailer
     public function __construct(MailerInterface $mailer, RecoveryPassword $recoveryPassword)
     {
         parent::__construct($mailer);
-        $this->from = 'apdcalculator@apd.fr';
+        $this->from = 'api@aeraulic.com';
         $this->subject = "apdcalculator - Password recovery - Do not reply";
         $this->contentType = 'html';
-        
+        $frontend_url = $_ENV['FRONTEND_URL'];
+        $frontendRecoverPasswordUri = $_ENV['FRONTEND_RECOVER_PASSWORD_URI'];
         
         $guid = $recoveryPassword->getGuid();
         $this->content = "
         <h1>Password recovery</h1>
         <p>You have request an password recovery.</p>
         <p>Click on the link below to choose a new password for your account</p>
-        <form action='http://localhost:8080/api/V2/updatepassword/$guid' method='POST' target='_blank'>
+        <form action='$frontend_url/$frontendRecoverPasswordUri/$guid' method='GET' target='_blank'>
             <input type='submit' value='Password recovery'>
         </form>
         ";
